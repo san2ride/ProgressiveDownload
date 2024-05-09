c6132cb458e89f685d73015e7c7bcab442baab0a
Jason <sanchezjason@me.com>
2024-05-09 16:39:20 -0600

    playground/async_sequence/progressive_download - async await, slower

---
+++ AsyncSeqProgressiveDWLD.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+extension URL {
+    func allLines() async -> Lines {
+        Lines(url: self)
+    }
+}
+
+struct Lines: Sequence {
+    
+    let url: URL
+    
+    func makeIterator() -> some IteratorProtocol {
+        
+        //MARK: downloading the data, split it and create new line. lines creates a subsequence
+        let lines = (try? String(contentsOf: url))?.split(separator: "\n") ?? []
+        return LinesIterator(lines: lines)
+    }
+}
+
+struct LinesIterator: IteratorProtocol {
+    
+    typealias Element = String
+    var lines: [String.SubSequence]
+    
+    //MARK: changing the lines array must be a mutating func
+    mutating func next() -> Element? {
+        if lines.isEmpty {
+            return nil
+        }
+        return String(lines.removeFirst())
+    }
+}
+
+let endpointURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv")!
+
+
+//MARK: awaiting for all data to comethrough, then data loads
+Task {
+    for line in await endpointURL.allLines() {
+        print(line)
+    }
+}
